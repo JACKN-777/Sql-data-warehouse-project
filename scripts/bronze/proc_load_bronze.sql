1c186716579222449f1f80fc1b9f839639bf6617
Harshit <harshit2401@gmail.com>
2026-07-20 21:46:27 +0530

    Update proc_load_bronze.sql with actions performed
    Added a section to describe actions performed by the stored procedure.

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -5,6 +5,7 @@ Stored Procedure: Load Bronze Layer (Source -> Bronze)
 Scripts Purpose:
     This stored procedure loads data into the 'bronze' schema from external CSV files.
     It performs the following actions:
+Action Performed:
     - Truncates the bronze tables before loading data.
     - Uses the 'BULK INSERT command to load data from csv Files to bronze tables.
 
